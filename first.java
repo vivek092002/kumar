8dd2a64fddb233ee3c27aa8cd49c3170f79f99b2
kumarvivek <kumar.vivek092002@gmail.com>
2024-10-03 15:10:41 +0530

    modified first.java

--- first.java
+++ first.java
@@ -1,5 +1,8 @@
 class first{
     public static void main(Strings[] args){
         System.out.print("Hello WOrld");
+        int a = 10;
+        int b = 11;
+        System.out.print(a+b);
     }
 }
